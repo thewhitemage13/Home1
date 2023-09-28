f034cbf0c725ae2cfcfe94bd9b89cc4f32da6a96
thewhitemage13 <144810207+thewhitemage13@users.noreply.github.com>
2023-09-28 11:53:21 +0200

    Add files via upload

---
+++ Home1/Home1.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+
+void Test(int a[],int dlina)
+{
+    int sum = 0;
+    float sr=0;
+    for (int i = 0; i < dlina; i++) {
+        sum += a[i];
+    }
+    sr = sum / dlina;
+    cout << sr;
+}
+
+int main()
+{
+    int a[] = { 10, 13, 14, 56, 78 };
+    int dlina = 5;
+    Test(a,dlina);
+}
